7becc53e154ceb0397d43375479df86fccb375fc
fahdlaslam <fahdlaslam@gmail.com>
2018-05-16 06:09:50 +0100

    -

---
+++ alfahd/assets/main.js
@@ -0,0 +1,1 @@
+$J = jQuery.noConflict();
---
+++ alfahd/assets/slideshow.js
@@ -0,0 +1,35 @@
+$(".slider").slick({
+
+  // normal options...
+  infinite: false,
+
+  // the magic
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+});
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
